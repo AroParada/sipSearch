fb61aa0d922a175b7212aeda1244e8dcd1249332
Harold parada <haroldparada20@gmail.com>
2024-08-15 03:51:07 -0400

    removed basename configuration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Search" element={<Search />} />
